016961ee9aeb7cbd64554a816e8b2a02cc46c78b
Julia Pap <papjuli@gmail.com>
2023-04-05 14:01:11 +0200

    add missing texts, change to scrollable text

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # rubber-band-method
+
+This is a reimplementation of a [program](https://web.archive.org/web/20071210071004/http://www.cs.elte.hu/~lovasz/tuttedemo.html) written orginally by **László Lovász**.
+
+This program illustrates how one simple mathematical idea -- replacing the edges of a graph by rubber bands -- can be used to design a variety of graph algorithms.
+
--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 import { loadGraph, parseGrf } from './graph.js'
 
-const topicElement = document.getElementById('topic-text')
-const nextButton = document.getElementById('next-btn')
-const prevButton = document.getElementById('prev-btn')
-
-let page = 0
+const topicTextElement = document.getElementById('topic-text')
 
 loadTopic('Intro')
-nextButton.onclick = nextPage
-document.getElementById('prev-btn').onclick = prevPage
 
 parseGrf("./graphs/dodecahedron.grf", loadGraph)
+
+
+function loadTopic(topicName) {
+  fetch(`./topics/${topicName}.html`)
+  .then(response => response.text())
+  .then((data) => {
+    topicTextElement.innerHTML = data
+  })
+}
 
 let loadTopicButtons = document.querySelectorAll("#loadTopicDropdown button")
 loadTopicButtons.forEach(btn => {
@@ -25,45 +28,3 @@ loadGraphButtons.forEach(btn => {
     parseGrf(`./graphs/${btn.dataset.name}.grf`, loadGraph)
   }
 })
-
-function loadTopic(topicName) {
-  fetch(`./topics/${topicName}.html`)
-  .then(response => response.text())
-  .then((data) => {
-    topicElement.innerHTML = data
-    page = 0
-    prevButton.disabled = true
-    if (topicElement.children.length == 1) {
-      nextButton.disabled = true
-    } else {
-      nextButton.disabled = false
-    }
-  })
-}
-
-function nextPage() {
-  if (page == topicElement.children.length - 1) return
-  topicElement.children[page].style.display = "none"
-  if (page == 0) {
-    prevButton.disabled = false
-  }
-  page++
-  topicElement.children[page].style.display = "block"
-  if (page == topicElement.children.length - 1) {
-    nextButton.disabled = true
-  }
-}
-
-function prevPage() {
-  if (page == 0) return
-  topicElement.children[page].style.display = "none"
-  if (page == topicElement.children.length - 1) {
-    nextButton.disabled = false
-  }
-  page--
-  topicElement.children[page].style.display = "block"
-  if (page == 0) {
-    prevButton.disabled = true
-  }
-}
-
